a9de76fc9a2803e9697f6634dfc68cbdfeab7fbc
AntonyJR <A.Reynolds@computer.org>
2021-06-25 01:09:02 +0000

    Added delete & get Integration and fixed stop Integration

---
+++ python/deleteIntegration.py
@@ -0,0 +1,36 @@
+import oci
+import argparse
+import os
+
+parser = argparse.ArgumentParser();
+parser.add_argument("--region", action="store", help="Region containing ocids")
+parser.add_argument("ocids", nargs="+", help="Integration Instance OCIDs")
+args = parser.parse_args();
+
+# Create a default config using DEFAULT profile in default location
+# Refer to
+# https://docs.cloud.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm#SDK_and_CLI_Configuration_File
+# for more info
+# Change region if provided as an argument
+config = oci.config.from_file()
+if args.region is not None :
+  config["region"] = args.region
+elif "region" not in config :
+  config["region"] = os.environ["OCI_REGION"]
+
+
+# Initialize service client with default config file
+integration_client = oci.integration.IntegrationInstanceClient(config)
+
+stop_responses = []
+for ocid in args.ocids :
+  # Send the request to service, some parameters are not required, see API
+  # doc for more info
+  try :
+    stop_responses.append((config["region"], ocid, integration_client.delete_integration_instance(integration_instance_id=ocid)))
+  except oci.exceptions.ServiceError as se :
+    print("{} {} {}".format(config["region"], ocid, se.message))
+
+print("Region OCID Status")
+for response in stop_responses :
+  print("{} {} {}".format(response[0], response[1], response[2].status))
---
+++ python/getIntegration.py
@@ -0,0 +1,34 @@
+import oci
+import argparse
+import os
+
+parser = argparse.ArgumentParser();
+parser.add_argument("--region", action="store", help="Region containing ocids")
+parser.add_argument("ocids", nargs="+", help="Integration Instance OCIDs")
+args = parser.parse_args();
+
+# Create a default config using DEFAULT profile in default location
+# Refer to
+# https://docs.cloud.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm#SDK_and_CLI_Configuration_File
+# for more info
+# Change region if provided as an argument
+config = oci.config.from_file()
+if args.region is not None :
+  config["region"] = args.region
+elif "region" not in config :
+  config["region"] = os.environ["OCI_REGION"]
+
+
+# Initialize service client with default config file
+integration_client = oci.integration.IntegrationInstanceClient(config)
+identity_client = oci.identity.IdentityClient(config)
+
+print("Region OCID Display_Name Lifecycle_State State_Message Compartment")
+for ocid in args.ocids :
+  # Send the request to service, some parameters are not required, see API
+  # doc for more info
+  try :
+    ii = integration_client.get_integration_instance(integration_instance_id=ocid).data
+    print("{} {} {} {} {} [{}]".format(config["region"], ocid, ii.display_name, ii.lifecycle_state, ii.state_message, identity_client.get_compartment(ii.compartment_id).data.name))
+  except oci.exceptions.ServiceError as se :
+    print("{} {} {}".format(config["region"], ocid, se.message))
